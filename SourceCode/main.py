00745356b8bc8587f44fc3d129deeec44801b64e
John Nguyen <78591393+jnguyen1111@users.noreply.github.com>
2021-08-22 01:34:52 -0500

    Update main.py

--- SourceCode/main.py
+++ SourceCode/main.py
@@ -1,141 +1,158 @@
 import pydirectinput
-import speech_recognition
 import time
-import pyaudio
 import sys
-import speech_recognition as sr
+import speech_recognition
+import pyaudio
+import pyautogui
 
-# obtain audio from the microphone
-r = sr.Recognizer()
+class character_movement:
+    # left to down for looking defines mouse relative positioning and where to move in pixels
+    def look_left(self):
+        pydirectinput.moveRel(-1500, 0, relative=True)
 
-def command_left(keys_pressed):
-    pydirectinput.moveRel(-325 , 0 , relative=True)
+    def look_right(self):
+        pydirectinput.moveRel(1500, 0, relative=True)
 
-def command_right(keys_pressed):
-    pydirectinput.moveRel(325 , 0 , relative=True)
+    def look_up(self):
+        pydirectinput.moveRel(0, -450, relative=True)
 
-def command_up(keys_pressed):
-    pydirectinput.moveRel(0 , -150 , relative=True)
+    def look_down(self):
+        pydirectinput.moveRel(0, 450, relative=True)
 
-def command_down(keys_pressed):
-    pydirectinput.moveRel(0, 150 , relative=True)
+    #looks back 180 degrees
+    def look_back(self):
+        pydirectinput.moveRel(3250, 0, relative=True)
 
-#make some for movement keys
+    def move_forward(self):
+        pydirectinput.keyDown("w")
+        time.sleep(1)
+        pydirectinput.keyUp("w")
 
-def walk(keys_pressed):
-    pydirectinput.keyDown("w")
-    keys_pressed.append("w")
+    def move_backward(self):
+        pydirectinput.keyDown("s")
+        time.sleep(1)
+        pydirectinput.keyUp("s")
 
-def jump(keys_pressed):
-    pydirectinput.keyDown("space")
-    keys_pressed.append("space")
+    def move_left(self):
+        pydirectinput.keyDown("a")
+        time.sleep(1)
+        pydirectinput.keyUp("a")
 
-def look_back(keys_pressed):
-    pydirectinput.moveRel(600 , 0 , relative=True)
+    def move_right( self):
+        pydirectinput.keyDown("d")
+        time.sleep(1)
+        pydirectinput.keyUp("d")
 
-def drop(keys_pressed):
-    pydirectinput.press("q")
+    #key press for crouching
+    def shift(self):
+        if not (self.player_crouched):
+            pydirectinput.keyDown("shift")
+            self.player_crouched = True
+        else:
+            pydirectinput.keyUp("shift")
+            self.player_crouched = False
 
-def shift(key_pressed):
-    #to do add a flag variable for this
-    pydirectinput.keyDown("shift")
+    # holds down walk key continously
+    def walk(self,keys_pressed):
+        pydirectinput.keyDown("w")
+        self.keys_pressed.append("w")
 
-    # if(not (player_crouched)):
-    #     pydirectinput.keyDown("shift")
-    #     player_crouched = True
-    # else:
-    #     pydirectinput.keyUp("shift")
-    #     player_crouched = False
+    def run(self,keys_pressed):
+        pydirectinput.keyDown("w")
+        pydirectinput.keyDown("r")
+        self.keys_pressed.append("w")
+        self.keys_pressed.append("r")
 
-def stop(keys_pressed):
-    for key in range(len(keys_pressed)):
-        pydirectinput.keyUp(keys_pressed[key])
-    keys_pressed.clear()
+    def jump(self,keys_pressed):
+        pydirectinput.keyDown("space")
+        self.keys_pressed.append("space")
 
-def left_click(keys_pressed):
-    pydirectinput.leftClick()
+    #press left mouse button and hold
+    def left_click_hold(self):
+        pyautogui.mouseDown()
 
-def right_click(keys_pressed):
-    pydirectinput.rightClick()
+    #press right mouse button and hold
+    def right_click_hold(self):
+        pyautogui.mouseDown(button="right")
 
-def teabag(keys_pressed):
-    for times in range(0,2):
-        pydirectinput.press("f5")
+    #press left mouse button once
+    def left_click(self):
+        pydirectinput.leftClick()
 
-    for times in range(0,3):
-        pydirectinput.keyDown("shift")
-        time.sleep(.3)
-        pydirectinput.keyUp("shift")
+    #press right mouse button once
+    def right_click(self):
+        pydirectinput.rightClick()
 
-    time.sleep(.3)
-    pydirectinput.press("f5")
+    #opens up the players inventory with items they have
+    def inventory(self):
+        pydirectinput.press("e")
 
-def run(keys_pressed):
-    pydirectinput.keyDown("w")
-    pydirectinput.keyDown("r")
-    keys_pressed.append("w")
-    keys_pressed.append("r")
+    #defines inventory number to switch to
+    def press_number_key(self,command):
+        pydirectinput.press(command)
 
-def inventory(keys_pressed):
-    pydirectinput.press("e")
+    #drops  current hotkey item
+    def drop(self):
+        pydirectinput.press("q")
 
-def quit_program(keys_pressed):
-    sys.exit()
+    #takes list of keys pressed and cancels it and unpresses the keys
+    def stop(self,keys_pressed):
+        for key in range(len(self.keys_pressed)):
+            pydirectinput.keyUp(self.keys_pressed[key])
+        self.keys_pressed.clear()
+        pyautogui.mouseUp(button="right")
+        pyautogui.mouseUp()
 
-def press_number_key(command):
-    pydirectinput.press(command)
+    def exit_program(self):
+        sys.exit()
 
-def move_forward(keys_pressed):
-    pydirectinput.keyDown("w")
-    time.sleep(1)
-    pydirectinput.keyUp("w")
+    #takes input from microphone and throws the audio source into googles speech api to text and calls for commands
+    def voice_listener(self):
+        while (True):
+            with self.speech_recognition.Microphone() as source:                            #gets microphone device/input
+                print("\nSay something!")
+                recording = self.audio.record(source, duration=3)                           #records audio for duration of three seconds
+            try:
+                command = self.audio.recognize_google(recording, language="en-US").lower()  #throw the recording into google speech api and get text back
+                print("User said: {}".format(command))
+                if command in self.run_walk_jump:                                           #check if the command are in dictionaries take value and form/call function
+                    self.run_walk_jump[command](self.keys_pressed)
+                elif command in self.voice_commands:
+                    self.voice_commands[command]()
+                elif command in self.number_commands:
+                    self.press_number_key(command)
+            except self.speech_recognition.UnknownValueError:
+                print("Google didnt get that try again")
+            except self.speech_recognition.RequestError:
+                print("Google services did not work try again")
 
-def move_backward(keys_pressed):
-    pydirectinput.keyDown("s")
-    time.sleep(1)
-    pydirectinput.keyUp("s")
+    def start_program(self):
+        for seconds in range(3, 0, -1):
+            print("Starting program in {}".format(seconds))
+            time.sleep(1)
+        self.voice_listener()
 
-def move_left(keys_pressed):
-    pydirectinput.keyDown("a")
-    time.sleep(1)
-    pydirectinput.keyUp("a")
-
-def move_right(keys_pressed):
-    pydirectinput.keyDown("d")
-    time.sleep(1)
-    pydirectinput.keyUp("d")
-
-number_commands = ["1","2","3","4","5","6","7","8","9","0"]
-
-voice_commands = {"look left":command_left , "look right":command_right , "look up": command_up , "look down": command_down ,
-                  "walk":walk , "quit":quit_program , "run":run , "stop":stop , "jump":jump , "left-click":left_click,
-                  "inventory":inventory , "right-click":right_click , "sneak":shift,"drop":drop,"look back":look_back,
-                  "tea bag":teabag, "move forward":move_forward,"move backward":move_backward , "move left": move_left,
-                  "move right": move_right}
-keys_pressed = []
-
-#slow down your speech for more accurate detection
-
-def voice_listener():
-    while(True):
-        with sr.Microphone() as source:
-            print("\nSay something!")
-            audio = r.listen(source, phrase_time_limit = 1.80)
-        try:
-            command = r.recognize_google(audio ,language="en-US").lower()
-            print("User said: {}".format(command))
-            if command in voice_commands:
-                voice_commands[command](keys_pressed)
-            elif command in number_commands:
-                press_number_key(command)
-        except speech_recognition.UnknownValueError:
-            print("Google didnt get that try again")
-        except speech_recognition.RequestError:
-            print("Google services did not work try again")
+    def __init__(self):
+        self.player_crouched = False                                                        #checks to see if the player is crouched or not
+        self.speech_recognition = speech_recognition
+        self.audio = self.speech_recognition.Recognizer()                                   #sets up audio settings for recording
+        self.number_commands = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]           #this is for inventory slot number in minecraft
+        self.keys_pressed = []                                                              #records key input so that when stop is called all keys are unpressed
+        self.run_walk_jump = { "walk": self.walk , "jump": self.jump , "run": self.run}     #these commands add to keys_pressed list which is seperated from voice_commands
+        self.voice_commands = {"look left": self.look_left, "look right": self.look_right,
+                               "look up": self.look_up, "look down": self.look_down,
+                                "exit program": self.exit_program,"stop": self.stop,
+                               "left-click": self.left_click,"inventory": self.inventory,
+                               "right-click": self.right_click,"couch": self.shift,
+                               "drop": self.drop, "look back": self.look_back,
+                               "forward": self.move_forward,
+                               "backward": self.move_backward, "left": self.move_left,
+                               "right": self.move_right ,"left hold":self.left_click_hold,
+                               "right hold":self.right_click_hold}
+        self.start_program()
 
 if __name__ == "__main__":
-    player_crouched = False
-    voice_listener()
+    player = character_movement()
 
 
 
